71ec046c5640a63c5c872469106970b054ccadf3
Vasu <vasukumar962@gmail.com>
2024-03-06 17:08:18 +0530

    Build the navbar

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,6 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div>Hey, I&apos;m vasu. For creating an issue click issues option on navigation bar.</div>
+    <div className='p-5'>Hey, I&apos;m vasu. <br /> For creating an issue click issues option on navigation bar.</div>
   )
 }
