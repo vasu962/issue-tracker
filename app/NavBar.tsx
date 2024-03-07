2c9704dc805926a2f434604cd660b3bdbc3fd97f
Vasu <vasukumar962@gmail.com>
2024-03-07 15:26:04 +0530

    set login button

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,39 +1,104 @@
 'use client';
 
-import React from 'react';
-import { usePathname } from 'next/navigation';
+import { Skeleton } from '@/app/components';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import React from 'react';
 import { AiFillBug } from 'react-icons/ai';
 import classnames from 'classnames';
+import { useSession } from 'next-auth/react';
+import {
+  Avatar,
+  Box,
+  Container,
+  DropdownMenu,
+  Flex,
+  Text,
+} from '@radix-ui/themes';
 
 const NavBar = () => {
+  return (
+    <nav className="font-medium mb-5 px-5 py-3">
+      <Container className='absolute'>
+        <Flex justify="between">
+          <Flex align="center" gap="3">
+            <Link href="/">
+              <AiFillBug className='h-5 w-5'/>
+            </Link>
+            <NavLinks />
+          </Flex>
+          <AuthStatus />
+        </Flex>
+      </Container>
+        <hr className="border-t border-gray-200 mt-10" />
+    </nav>
+  );
+};
+
+const NavLinks = () => {
   const currentPath = usePathname();
+
   const links = [
     { label: 'Dashboard', href: '/' },
-    { label: 'Issues', href: '/issues' },
+    { label: 'Issues', href: '/issues/new' },
   ];
 
   return (
-    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
-      <Link href="/">
-        <AiFillBug className="h-6 w-6" />
-      </Link>
-      <ul className="flex space-x-6 font-medium">
+      <ul className="flex space-x-6">
         {links.map((link) => (
-          <Link
-            key={link.href}
-            className={classnames({
-              'text-zinc-900': link.href === currentPath,
-              'text-gray-500': link.href !== currentPath,
-              'hover:text-zinc-800 transition-colors': true,
-            })}
-            href={link.href}
-          >
-            {link.label}
-          </Link>
+          <li key={link.href}>
+            <Link
+              className={classnames({
+                // "nav-link": true,
+                '!text-zinc-900': link.href === currentPath,
+                '!text-zinc-500': link.href !== currentPath,
+                'hover:text-zinc-200 transition-colors': true,
+              })}
+              href={link.href}
+            >
+              {link.label}
+            </Link>
+          </li>
         ))}
       </ul>
-    </nav>
+  );
+};
+
+const AuthStatus = () => {
+  const { status, data: session } = useSession();
+
+  if (status === 'loading') return <Skeleton width="3rem" />;
+
+  if (status === 'unauthenticated')
+    return (
+      <Link className="fixed top-0 right-0 m-3 px-5" href="/api/auth/signin">
+        Login
+      </Link>
+    );
+
+  return (
+    <Box>
+      <DropdownMenu.Root>
+        <DropdownMenu.Trigger>
+          <Avatar
+            src={session!.user!.image!}
+            fallback="?"
+            size="2"
+            radius="full"
+            className="cursor-pointer"
+            referrerPolicy="no-referrer"
+          />
+        </DropdownMenu.Trigger>
+        <DropdownMenu.Content>
+          <DropdownMenu.Label>
+            <Text size="2">{session!.user!.email}</Text>
+          </DropdownMenu.Label>
+          <DropdownMenu.Item>
+            <Link href="/api/auth/signout">Log out</Link>
+          </DropdownMenu.Item>
+        </DropdownMenu.Content>
+      </DropdownMenu.Root>
+    </Box>
   );
 };
 
---
+++ app/components/index.ts
@@ -0,0 +1,3 @@
+export { default as Spinner } from './Spinner';
+export { default as Skeleton } from './Skeleton';
+export { default as ErrorMessage } from './ErrorMessage';
